57eaf4dcea1cf5d0e6c63080838aa3eb93d125b5
Aaron Moffatt <a@airvr.studio>
2023-09-21 09:00:41 +0200

    Add compiled Gaussian Splatting viewers and update script to rebuild them

--- gaussian-splatting-build/bin/rebuild-viewers.sh
+++ gaussian-splatting-build/bin/rebuild-viewers.sh
@@ -5,8 +5,11 @@ git clone https://github.com/amoffatt/gaussian-splatting --recursive
 sudo apt-get install -y -q libglew-dev libassimp-dev libboost-all-dev libgtk-3-dev libopencv-dev libglfw3-dev libavdevice-dev libavcodec-dev libeigen3-dev libxxf86vm-dev libembree-dev
 
 # Install CUDA toolkit 11.8
-wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda_11.8.0_520.61.05_linux.run
-sudo sh cuda_11.8.0_520.61.05_linux.run
+#wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda_11.8.0_520.61.05_linux.run
+#sudo sh cuda_11.8.0_520.61.05_linux.run
+
+export PATH=/usr/local/cuda-11.8/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-11.0/lib64:$LD_LIBRARY_PATH
 
 # Build Interactive Viewers
 cd gaussian-splatting/SIBR_viewers
