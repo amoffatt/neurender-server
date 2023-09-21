f8aee755f6fac008a360282ea31eb13bce3cbad4
Aaron <a@airvr.studio>
2023-09-21 05:02:22 +0200

    Work on scripts to build Gaussian Splatting viewers

--- gaussian-splatting-build/bin/rebuild-submodules.sh
+++ gaussian-splatting-build/bin/rebuild-submodules.sh
@@ -3,5 +3,3 @@
 git clone https://github.com/amoffatt/gaussian-splatting --recursive
 pip install -r gaussian-splatting/nerfstudio-requirements.txt
 
-
-
---
+++ gaussian-splatting-build/bin/rebuild-viewers.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+git clone https://github.com/amoffatt/gaussian-splatting --recursive
+
+sudo apt-get install -y -q libglew-dev libassimp-dev libboost-all-dev libgtk-3-dev libopencv-dev libglfw3-dev libavdevice-dev libavcodec-dev libeigen3-dev libxxf86vm-dev libembree-dev
+
+# Install CUDA toolkit 11.8
+wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda_11.8.0_520.61.05_linux.run
+sudo sh cuda_11.8.0_520.61.05_linux.run
+
+# Build Interactive Viewers
+cd gaussian-splatting/SIBR_viewers
+cmake -Bbuild . -DCMAKE_BUILD_TYPE=Release # add -G Ninja to build faster
+cmake --build build -j24 --target install
+
+
+
